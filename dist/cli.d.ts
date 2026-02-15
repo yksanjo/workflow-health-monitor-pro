7b2057f589fff94c8d264d3a8d803f5c649e9a20
Yoshi Kondo <yksanjo@gmail.com>
2026-02-14 19:54:36 -0500

    Add .gitignore

--- dist/cli.d.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-/**
- * CLI Interface for Workflow Health Monitor Pro
- */
-export {};
-//# sourceMappingURL=cli.d.ts.map
--- dist/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * Workflow Health Monitor Pro - Type Definitions
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=types.js.map
